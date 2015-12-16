df8834e51acfa042537138d1603c45a6936e3f83
urizev <urizev@gmail.com>
2015-12-16 16:44:19 +0100

    Missing import

--- src/com/urizev/gpx/GPXParser.java
+++ src/com/urizev/gpx/GPXParser.java
@@ -32,6 +32,7 @@ import java.util.Iterator;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
